68c4acd7c684f4378236d38d437473b0dd300505
sergeygutovskiy <sergey.gutovsk@gmail.com>
2020-08-21 14:57:41 +0300

    nav, header, gallery, about, contacts ready

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,82 +1,164 @@
 var curBot = 0;
+var scrollable = true;
+var stY;
+
+var pageTitles = [
+	"",
+	"Галерея",
+	"О нас",
+	"Контакты"
+];
+
+var pageURIs = [
+	"",
+	"#gallery",
+	"#about-us",
+	"#contacts"
+];
+
+var pageStatuses = [
+	"viewing-header",
+	"viewing-gallery",
+	"viewing-about",
+	"viewing-contacts"
+];
+
+function whenScroll(up = false) 
+{
+	// change page title
+	
+	anime({
+		targets: '.nav__page-title',
+		translateY: 40,
+		opacity: 0,
+		easing: 'easeInOutExpo',
+		duration: 400
+	});
+	
+	setTimeout(() => {
+		$(".nav__page-title").text(pageTitles[curBot / 100]);
+		
+		anime({
+			targets: '.nav__page-title',
+			translateY: 0,
+			opacity: 1,
+			easing: 'easeInOutExpo',
+			duration: 800
+		});	
+	}, 400);
+
+	// about page counters
+	
+	if (pageTitles[curBot / 100] == "О нас")
+	{
+		setTimeout(startCounters, 600); 
+	}
+
+	// change uri
+	
+	window.location.hash = pageURIs[curBot / 100];
+
+	// change current page for pagination bar
+	
+	if (up)
+	{
+		paginationPrev();	
+		$("body").removeClass(pageStatuses[curBot / 100 + 1]);
+	}
+	else
+	{
+		paginationNext();
+		$("body").removeClass(pageStatuses[curBot / 100 - 1]);	
+	}
+
+	// change body's page-viewing status
+	
+	if (window.innerWidth >= mobileWidth || ((window.innerWidth < mobileWidth) && (curBot / 100) != 0) )
+		$("body").addClass(pageStatuses[curBot / 100]);
+
+}
 
 $(function() {
-	var scrollable = true;
 
-	function navDown() {
-		curBot += 100;
+	if (window.innerWidth >= mobileWidth)
+		$("body").addClass(pageStatuses[0]);
 
-		paginationNext();
+});
 
-		scrollable = false;
-		$(".backgrounds").css("transform", "translateY(-" + curBot + "%)");
-		setTimeout(function() {
-			scrollable = true
-		}, 1000);
+function navDown() {
+	curBot += 100;
 
-		if (curBot == 100)
-		{
-			hidePagination();
-			$(".body-blur").css("opacity", "1");
-		}
-		else if (curBot == 200)
-		{
-			showPagination();
-			// $(".body-background").css("filter", "blur(0)");
-		}
-	}
+	whenScroll();
+	
+	scrollable = false;
+	$(".backgrounds").css("transform", "translateY(-" + curBot + "%)");
+	setTimeout(function() {
+		scrollable = true
+	}, 1000);
 
-	function navUp() {
-		curBot -= 100;
+	// if (curBot == 100)
+	// {
+	//	hidePagination();
+	//	$(".body-blur").css("opacity", "1");
+	// }
+	// else if (curBot == 200)
+	// {
+	//	showPagination();
+	//	
+	//	setTimeout(() => {
+	//		startCounters();
+	//	}, 800);
+	// }
+}
 
-		paginationPrev();
+function navUp() {
+	curBot -= 100;
 
-		scrollable = false;
-		$(".backgrounds").css("transform", "translateY(-" + curBot + "%)");
-		setTimeout(function() {
-			scrollable = true
-		}, 1000);
+	whenScroll(true);
 
-		if (curBot == 0)
-		{
-			showPagination();
-			 $(".body-blur").css("opacity", "0");
-		}
-		else if (curBot == 100)
-		{
-			$(".body-background").css("transform", "translateY(0)");
-			hidePagination();
-			// $(".body-background").css("filter", "blur(8px)");
-		}
-	}
-	var stY;
+	scrollable = false;
+	$(".backgrounds").css("transform", "translateY(-" + curBot + "%)");
+	setTimeout(function() {
+		scrollable = true
+	}, 1000);
 
-	$(document).on("touchstart", function(event) {
-		stY = event.originalEvent.touches[0].screenY;
-	})
+	// if (curBot == 0)
+	// {
+	//	showPagination();
+	//	 $(".body-blur").css("opacity", "0");
+	// }
+	// else if (curBot == 100)
+	// {
+	//	$(".body-background").css("transform", "translateY(0)");
+	//	hidePagination();
+	//	// $(".body-background").css("filter", "blur(8px)");
+	// }
+}
 
-	$(document).on("touchmove", function(event) {
-		// event.preventDefault();
-		var curY = event.originalEvent.touches[0].screenY;
-		if (stY - curY > 50 && curBot <= 300 && scrollable)
-			navDown();
-		else if (curY - stY > 50 && curBot > 0 && scrollable) 
-			navUp();
-	})
+$(document).on("touchstart", function(event) {
+	stY = event.originalEvent.touches[0].screenY;
+});
 
-	$(document).on("keydown", function(event) {
-		if (event.keyCode == 40 && curBot <= 300 && scrollable) 
-			navDown();
-		else if (event.keyCode == 38 && curBot > 0 && scrollable)
-			navUp();
-	})
+$(document).on("touchmove", function(event) {
+	// event.preventDefault();
+	var curY = event.originalEvent.touches[0].screenY;
+	if (stY - curY > 50 && curBot <= 300 && scrollable)
+		navDown();
+	else if (curY - stY > 50 && curBot > 0 && scrollable) 
+		navUp();
+});
 
-	$(document).on("mousewheel DOMMouseScroll", function(event) {
-		// event.preventDefault();
-		if ((event.originalEvent.wheelDelta < 0 || event.originalEvent.detail > 0) && curBot <= 300 && scrollable)
-			navDown();
-		else if ((event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) && curBot > 0 && scrollable)
-			navUp();    
-	})
+$(document).on("keydown", function(event) {
+	if (event.keyCode == 40 && curBot <= 300 && scrollable) 
+		navDown();
+	else if (event.keyCode == 38 && curBot > 0 && scrollable)
+		navUp();
+});
 
-})
+$(document).on("mousewheel DOMMouseScroll", function(event) {
+	// event.preventDefault();
+	if ((event.originalEvent.wheelDelta < 0 || event.originalEvent.detail > 0) && curBot <= 300 && scrollable)
+		navDown();
+	else if ((event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) && curBot > 0 && scrollable)
+		navUp();    
+});
---
+++ js/numbers.js
@@ -0,0 +1,35 @@
+var timeout = 1;
+var counters = null;
+
+counters = $('.counter');
+
+counters.each(function () {
+	var that = $(this), num = that.html();
+	that.html(0);
+	
+	console.log(1);
+
+	that.attr('data-num',num);
+});
+
+
+function startCounters()
+{
+	counters.each(function() {
+		var that = $(this);
+		if (!that.data('start')) 
+		{
+			that.attr('data-start', true);
+			var i = 1, num = that.data('num'), step = Math.round(1000 * timeout / num),
+			int = setInterval(() => {
+				if (i <= num) {
+					 that.html(i);
+				}
+				else {
+					clearInterval(int);
+				}
+				i++;
+			},step);
+		}
+	});
+}
