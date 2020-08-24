71cec6bd0f08d399d671c9201bf0bf1b8cc554c8
sergeygutovskiy <sergey.gutovsk@gmail.com>
2020-08-24 17:35:39 +0300

    final? no

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "EvaBorisova2018@yandex.ru";
+$sitename = "ЕваБорисова";
+
+$name = trim($_POiST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$message = "Имя: $name \nТелефон: $phone \nПочта: $email";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
